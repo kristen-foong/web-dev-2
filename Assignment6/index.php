5cb05c709b6a128927aa384166d98226698bbc55
Kristen Foong <kristen.foong@gmail.com>
2018-10-24 11:36:05 -0700

    got favs sort of working

--- Assignment6/index.php
+++ Assignment6/index.php
@@ -27,24 +27,40 @@ if ($conn->connect_error) {
     die("He's dead, Jim! " . $conn->connect_error);
 }
 
+$result = $conn->query("call pokedex()");
+$table = $result->fetch_all(MYSQLI_ASSOC);
+
+if($result){
 //start session
 session_start();
-$_SESSION["fav"] = [];
+if(!isset($_SESSION["fav"])){
+    $_SESSION["fav"] = [];
+}
 // generate page
-if(empty($_SESSION["fav"])){
-  $result = $conn->query("call pokedex()");
-  $table = $result->fetch_all(MYSQLI_ASSOC);
-  $template = $twig->load('page1.twig.html');
-  echo $template->render(array("title"=>"pokedex", "pokedex"=>$table, "fav"=>$_SESSION["fav"]));
-  $conn->close();
+if(isset($_GET["fav"]) && isset($_SESSION["fav"])){
+  if(sizeof($_SESSION["fav"]) <= 6) {
+    $newfav = $_GET["fav"];
+    array_push($_SESSION["fav"],$newfav);
+  }
 }
-else {
-  $fav = $_GET[$_SESSION["fav"]];
-  $result = $conn->query("call sort_type('$typename')");
-  $table = $result->fetch_all(MYSQLI_ASSOC);
-  $template = $twig->load('page1.twig.html');
-  echo $template->render(array("title"=>"pokedex","pokemon"=>$table, "fav"=>$fav));
-  $conn->close();
+if(isset($_GET["unfav"]) && isset($_SESSION["fav"])){
+  $_SESSION["fav"] = array_diff($_SESSION["fav"],array($_GET["unfav"]));
 }
+
+$favarr = [];
+
+foreach($_SESSION["fav"] as $favorite) {
+  foreach($table as $row) {
+    if($favorite == $row["name"]){
+      array_push($favarr,$row);
+    }
+  }
+}
+
+$template = $twig->load('page1.twig.html');
+echo $template->render(array("title"=>"pokedex", "pokedex"=>$table, "fav"=>$favarr));
+$conn->close();
+}
+
 
 ?>
